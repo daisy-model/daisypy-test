50ff34d35eda8859644b2b96bbedd2353f281e4c
Silas <silas@di.ku.dk>
2024-04-10 15:00:13 +0200

    Dont emit warnings, instead return errors, not_similar and not_identical
    This makes is easier to print a nice summary at the end, where errors
    not similar files, and not identical files can be grouped.

--- daisypy/test/compare_dlf_files.py
+++ daisypy/test/compare_dlf_files.py
@@ -1,5 +1,4 @@
 '''Compare two dlf files using smallest meaningfull levels'''
-import os
 import warnings
 
 from pint.errors import UndefinedUnitError, DimensionalityError
@@ -16,7 +15,7 @@ __all__ = [
 def compare_dlf_files(path1, path2,
                       skip_header=frozenset(('VERSION', 'RUN', 'SIMFILE')),
                       precision=1e-8,
-                      sml_warn_level=0.001):
+                      sml_identity_threshold=0.001):
     '''Compare two dlf files
 
     Parameters
@@ -28,39 +27,37 @@ def compare_dlf_files(path1, path2,
       Header keys to exclude from comparison
 
     precision: float
-      Consider two numbers equal if their absolute difference is less than this value
+      Consider two numbers equal if their absolute difference is less than this value. Only used if
+      no SML is available
 
-    sml_warn_level: float
-      Emit a warning if the absolute difference between two numeric values is larger than this
-      value times the corresponding sml
+    sml_identity_threshold: float
+      Consider two values identical if their difference is less than sml_identity_threshold * sml
 
     Returns
     -------
-    True if the files compare equal, False otherwise
+    (errors, not_similar, not_identical) 
+      errors: list of str
+      not_similar: list of str
+      not_identical: list of str
     '''
     dlf1 = read_dlf(path1)
     dlf2 = read_dlf(path2)
 
-    # Check headers match
     diff_headers = _compare_headers(dlf1.header, dlf2.header, skip_header)
     if len(diff_headers) > 0:
-        return False
+        return [], diff_headers, []
 
     diff_units = _compare_units(dlf1.units, dlf2.units)
     if len(diff_units) > 0:
-        return False
+        return [], diff_units, []
 
-
-    sml_names = [os.path.basename(path1), dlf1.header['dlf-component']]
-    diff_bodies = _compare_bodies(dlf1.body,
-                                  dlf2.body,
-                                  dlf1.units,
-                                  sml_names,
-                                  precision,
-                                  sml_warn_level)
-    if len(diff_bodies) > 0:
-        return False
-    return True
+    
+    return _compare_bodies(dlf1.body,
+                           dlf2.body,
+                           dlf1.units,
+                           dlf1.header,
+                           precision,
+                           sml_identity_threshold)
 
 def _compare_headers(h1, h2, skip_header):
     diff = []
@@ -84,15 +81,17 @@ def _compare_units(u1, u2):
             diff.append((k, u1[k], u2[k]))
     return diff
 
-def _compare_bodies(b1, b2, units, sml_names, precision, sml_warn_level):
+def _compare_bodies(b1, b2, units, header, precision, sml_identity_threshold):
     # pylint: disable=too-many-arguments
-    bad_diff = []
+    not_similar = []
+    not_identical = []
+    errors = []
     try:
         diff = b1.compare(b2)
         if len(diff) > 0:
-            sml_map = load_smallest_meaningful_level(sml_names, daisy_ureg)
+            sml_map = load_smallest_meaningful_level(header, daisy_ureg)
             if len(sml_map) == 0:
-                warnings.warn(f'No SML definitions loaded. Tried {sml_names}')
+                warnings.warn('No SML definitions loaded.')
             for col in diff.columns.levels[0]:
                 abs_delta = (diff[col]['self'] - diff[col]['other']).abs()
                 max_abs_delta_idx = abs_delta.argmax()
@@ -100,30 +99,30 @@ def _compare_bodies(b1, b2, units, sml_names, precision, sml_warn_level):
                 try:
                     sml = sml_map[col]
                     delta_u = (delta * dlf_unit_to_pint_unit(units[col], daisy_ureg)).to(sml)
-                    if delta_u > sml:
-                        bad_diff.append((col, b1[col].iloc[max_abs_delta_idx], b2[col].iloc[max_abs_delta_idx]))
-                        print(f'ERROR: {delta_u} > {sml} for {col}')
-                        print(diff[col].iloc[max_abs_delta_idx])
-                    elif delta_u > sml_warn_level * sml:
-                        warnings.warn(f'{delta_u} > {sml_warn_level * sml} for {col}')
+                    if delta_u > sml_identity_threshold * sml:
+                        row = diff[col].iloc[max_abs_delta_idx]
+                        msg = f'[{col}]: {row["self"]} | {row["other"]} | {delta_u} | {sml}'
+                        if delta_u > sml:
+                            not_similar.append(msg)
+                        else:
+                            not_identical.append(msg)
                 except KeyError:
                     # We dont have an SML
-                    if delta > precision:
-                        bad_diff.append((col, b1[col].iloc[max_abs_delta_idx], b2[col].iloc[max_abs_delta_idx]))
-                        print(f'ERROR: {delta} > {precision} for {col}')
-                        print(diff[col].iloc[max_abs_delta_idx])
+                    warnings.warn(f'No SML for {col}')
+                    if delta > sml_identity_threshold * precision:
+                        row = diff[col].iloc[max_abs_delta_idx]
+                        msg = f'[{col}]: {row["self"]} | {row["other"]} | {delta} | {precision}'
+                        if delta > precision:
+                            not_similar.append(msg)
+                        else:
+                            not_identical.append(msg)
                 except UndefinedUnitError:
-                    error = f'ERROR: Unknown unit {units[col]} for {col}'
-                    print(error)
-                    bad_diff.append((error, '', ''))
+                    errors.append(f'Unknown unit {units[col]} for {col}')
                 except AttributeError:
-                    error = f'ERROR: Unit conversion error {units[col]} for {col}'
-                    print(error)
-                    bad_diff.append((error, '', ''))
+                    errors.append(f'Unit conversion error {units[col]} for {col}')
                 except DimensionalityError:
-                    error = f'ERROR: Unit mismatch {units[col]} !~ {sml_map[col]} for {col}'
-                    print(error)
-                    bad_diff.append((error, '', ''))
+                    errors.append(f'Unit mismatch {units[col]} !~ {sml_map[col]} for {col}')
     except ValueError as e:
-        bad_diff.append((e, '', ''))
-    return bad_diff
+        errors.append(e)
+
+    return errors, not_similar, not_identical
--- daisypy/test/test_daisy.py
+++ daisypy/test/test_daisy.py
@@ -22,57 +22,79 @@ def main():
     parser.add_argument('out_dir', type=str, help='Output directory for errors')
     parser.add_argument('--no-warnings', action='store_true',
                         help='If set do not emit warnings from SML comparisons')
-    parser.add_argument('--sml-warn-level', type=float, default=0.001,
-                        help='Emit a warning if difference larger than `sml_warn_level` * sml')
+    parser.add_argument('--sml-identity-threshold', type=float, default=0.001,
+                        help='Consider two values identical if their difference is less than '
+                        '`sml_identity_threshold` * sml')
     parser.add_argument('--default-float-epsilon', type=float, default=1e-8, help='''Pass numeric
     comparison if absolute difference is less than this value. Only used if no SML is defined''')
+    parser.add_argument('--path', type=str, help='Add to path when running daisy', default='.')
     args = parser.parse_args()
 
     if args.no_warnings:
         warnings.showwarning = lambda message, *args: message
     else:
-        warnings.showwarning = lambda message, *args: print('WARNING:', message, file=sys.stderr)
+        warnings.showwarning = lambda message, category, *args: \
+            print(category.__name__, message, file=sys.stderr)
 
     with tempfile.TemporaryDirectory() as tmpdir:
-        daisy_args = [args.daisy_binary, args.program, '-d', tmpdir, '-q']
+        daisy_args = [args.daisy_binary, '-q', '-d', tmpdir, '-D', args.path, args.program]
         print(' '.join(daisy_args))
         result = subprocess.run(daisy_args, check=False)
         if result.returncode != 0:
             print('ERROR: Daisy execution failed', file=sys.stderr)
             return 1
 
-        errors, mismatch = [], []
+        errors, not_similar, not_identical = [], [], []
         for entry in os.scandir(args.reference_dir):
             if entry.is_file():
                 new_file_path = os.path.join(tmpdir, entry.name)
                 if not os.path.exists(new_file_path):
-                    print(f'{new_file_path} does not exist')
-                    errors.append(entry.name)
+                    errors.append((entry.name, [f'{new_file_path} does not exist']))
                 else:
-                    match = True
                     file_type = os.path.splitext(entry.name)[-1]
-                    if file_type == '.dlf':
-                        match = compare_dlf_files(entry.path, new_file_path,
-                                                  precision=args.default_float_epsilon,
-                                                  sml_warn_level=args.sml_warn_level)
-                    else:
+                    if file_type != '.dlf':
                         warnings.warn(f'Skipping file type {file_type}')
-                    if not match:
-                        mismatch.append(entry.name)
-                        os.makedirs(args.out_dir, exist_ok=True)
-                        error_file_path = os.path.join(args.out_dir, f'error_{entry.name}')
-                        ref_file_path = os.path.join(args.out_dir, f'ref_{entry.name}')
-                        shutil.copy(new_file_path, error_file_path)
-                        shutil.copy(entry.path, ref_file_path)
-
+                    else:
+                        failed = False
+                        err, not_sim, not_id = compare_dlf_files(
+                            entry.path,
+                            new_file_path,
+                            precision=args.default_float_epsilon,
+                            sml_identity_threshold=args.sml_identity_threshold
+                        )
+                        if len(err) > 0:
+                            errors.append((entry.name, err))
+                            failed = True
+                        if len(not_sim) > 0:
+                            not_similar.append((entry.name, not_sim))
+                            failed = True
+                        if len(not_id) > 0:
+                            not_identical.append((entry.name, not_id))
+                            failed = True
+                        if failed:
+                            os.makedirs(args.out_dir, exist_ok=True)
+                            error_file_path = os.path.join(args.out_dir, f'error_{entry.name}')
+                            ref_file_path = os.path.join(args.out_dir, f'ref_{entry.name}')
+                            shutil.copy(new_file_path, error_file_path)
+                            shutil.copy(entry.path, ref_file_path)
+    status = 0
     if len(errors) > 0:
-        print('== Errors ==', *errors, sep='\n')
-        return 2
+        print('== Errors ==')
+        for name, err in errors:
+            print(name, *errors, sep='\n\t', end='\n\n')
+        status += 1
 
-    if len(mismatch) > 0:
-        print('== Mismatches ==', *mismatch, sep='\n')
-        return 4
-    return 0
+    if len(not_similar) > 0:
+        print('== Not similar ==')
+        for name, not_sim in not_similar:
+            print(name, *not_sim, sep='\n\t', end='\n\n')
+        status += 2
+
+    if len(not_identical) > 0:
+        print('== Not identical ==')
+        for name, not_id in not_identical:
+            print(name, *not_id, sep='\n\t', end='\n\n')
+    return status
 
 if __name__ == '__main__':
     sys.exit(main())
