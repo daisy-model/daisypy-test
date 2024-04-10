595295d14f8972584e4ec91c37ea4a8b0ab4283e
Silas Ørting <silas@di.ku.dk>
2024-04-10 15:44:12 +0200

    Cleaner output (#6)
    * Dont emit warnings, instead return errors, not_similar and not_identical
    
    This makes is easier to print a nice summary at the end, where errors
    not similar files, and not identical files can be grouped.
    
    * Default chemical SML + specific for chemical N
    
    * Add a bit of info about SML definitions

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # daisypy-test
-Test framework for daisy
+Test framework for daisy log files (.dlf)
+
+
+## Smallest meaningfull level
+The smallest meaningful level (SML) for a particular value is the smallest scale at which differences are meaningful. For example, the SML for field nitrogen is 1 kg/ha, which means that differences less than 1 kg/ha are ignored.
+
+An SML for a specific log is defined in a `.toml` file in `daisypy/test/sml-definitions`. Mapping from log names to SMLs are done in `daisypy/test/sml.py`. Log names are specified when the log is defined, `(deflog "log name" ...`.
+
+There are general SML definitions for chemicals, but this might not work for specific chemicals. To define an SML for a specific chemical you should add a mapping with the name `log-name chemical-name`. For example, to add an SML definition for "N" in the "Field chemical" log you should a mapping named `Field chemical N`.
--- daisypy/test/sml.py
+++ daisypy/test/sml.py
@@ -1,7 +1,6 @@
 '''Smallest meaningful levels (SMLs) are used during testing. Differences less than 1 SML are
 considered irrelevant for testing purposes.'''
 import os
-import warnings
 import tomllib
 
 import pint
@@ -10,28 +9,39 @@ __all__ = [
     'load_smallest_meaningful_level'
 ]
 
-def load_smallest_meaningful_level(names, ureg):
+
+sml_paths = {
+    'Field chemical' : 'sml-definitions/field_chemicals.toml',
+    'Soil chemical' : 'sml-definitions/soil_chemicals.toml',
+    
+    'Field chemical N' : 'sml-definitions/field_chemicals_N.toml',
+    'Soil chemical N' : 'sml-definitions/soil_chemicals_N.toml',
+    
+    'Field nitrogen' : 'sml-definitions/field_nitrogen.toml',
+    'Soil nitrogen' : 'sml-definitions/soil_nitrogen.toml',
+    
+    'Soil water' : 'sml-definitions/soil_water.toml',
+    'Field water' : 'sml-definitions/field_water.toml',
+    
+    'Crop' : 'sml-definitions/crop.toml',
+}
+
+def load_smallest_meaningful_level(header, ureg):
     '''Load smallest meaningful level (SML) definition.
 
     Parameters
     ----------
-    names :list of str OR str
-      Either a list of SML definition names or a single SML definition name
+    header: dict
+      Header from a dlf file
 
     Returns
     -------
     dict of SML definitions or empty dict if unable to load
     '''
-    sml_paths = {
-        'harvest' : 'sml-definitions/harvest.toml',
-        'Field nitrogen' : 'sml-definitions/field_nitrogen.toml',
-        'Soil nitrogen' : 'sml-definitions/soil_nitrogen.toml',
-        'Soil water' : 'sml-definitions/soil_water.toml',
-        'Field water' : 'sml-definitions/field_water.toml',
-        'Crop' : 'sml-definitions/crop.toml',
-    }
-    if not isinstance(names, list):
-        names = [names]
+    names = []
+    if 'chemical' in header['dlf-component'] and 'CHEMICAL' in header:
+        names.append(f"{header['dlf-component']} {header['CHEMICAL']}")
+    names.append(header['dlf-component'])
     for name in names:
         try:
             path = os.path.join(os.path.dirname(os.path.realpath(__file__)), sml_paths[name])
@@ -39,8 +49,7 @@ def load_smallest_meaningful_level(names, ureg):
                 sml = tomllib.load(infile)
             for k, v in sml.items():
                 sml[k] = ureg(v)
-            print(f"INFO: Found SML definition for '{name}'", flush=True)
             return sml
-        except (KeyError, pint.UndefinedUnitError) as e:
-            warnings.warn(f'Could not load SML definition for {name}: {e}')
+        except (KeyError, pint.UndefinedUnitError):
+            pass
     return {}
